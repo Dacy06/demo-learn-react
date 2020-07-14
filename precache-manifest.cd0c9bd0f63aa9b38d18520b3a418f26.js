fcb9397e6ff50933d6be0f580b935f33cea4169e
zhangwenjie16 <zhangwenjie16@meituan.com>
2020-07-14 17:01:33 +0800

    Updates

---
+++ precache-manifest.cd0c9bd0f63aa9b38d18520b3a418f26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3c6a40c248e434d3c41c1d03c8a42b",
+    "url": "/demo-learn-react/index.html"
+  },
+  {
+    "revision": "7653b142e3d5621de979",
+    "url": "/demo-learn-react/static/css/main.5613a102.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/demo-learn-react/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-learn-react/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7653b142e3d5621de979",
+    "url": "/demo-learn-react/static/js/main.9cbfa56b.chunk.js"
+  },
+  {
+    "revision": "7255a0f71190b2af9ed4",
+    "url": "/demo-learn-react/static/js/runtime-main.aeac7c52.js"
+  }
+]);
